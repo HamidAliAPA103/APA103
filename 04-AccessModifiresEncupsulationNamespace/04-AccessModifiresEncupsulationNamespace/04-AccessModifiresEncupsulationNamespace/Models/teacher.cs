28dcdf6493027692a798ca780f18a05152a13302
unknown <mahammadva-bpa203@code.edu.az>
2026-03-18 00:18:19 +0400

    04-AccessModifiresEncupsulationNamespace

---
+++ 04-AccessModifiresEncupsulationNamespace/04-AccessModifiresEncupsulationNamespace/04-AccessModifiresEncupsulationNamespace/Models/teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_AccessModifiresEncupsulationNamespace.Models
+{
+    public  class Teacher
+    {
+        public void Test()
+        {
+            Person person =  new Person(" ");
+            person.name = "Test";
+        }
+    }
+}
---
+++ 04-AccessModifiresEncupsulationNamespace/04-AccessModifiresEncupsulationNamespace/04-AccessModifiresEncupsulationNamespace/Program.cs
@@ -0,0 +1,20 @@
+﻿using _04_AccessModifiresEncupsulationNamespace.Models;
+
+namespace _04_AccessModifiresEncupsulationNamespace
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student = new Student(" ");
+            //student.name = "test";
+            //Console.WriteLine(student.name); 
+
+
+
+            Car car = new Car();
+            car.horsepower = 200;
+            Console.WriteLine(car.horsepower);
+        }
+    }
+}
