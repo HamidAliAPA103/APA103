2a9dc9d5c3ff4698ac447c3df224928cb3419c8d
unknown <mahammadva-bpa203@code.edu.az>
2026-05-05 00:19:52 +0400

    25-MVC Intro

---
+++ 25-MVC Intro/25-MVC Intro/25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return "APA103";
+
+
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+            return RedirectToAction(nameof(Index),"Product");
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
